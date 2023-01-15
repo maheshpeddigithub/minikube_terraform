d48f26590659876f96e0a1c010dae3277ec7e324
Mahesh Peddi <maheshbabu.peddi@googlecloud.corp-partner.google.com>
2023-01-15 00:56:13 +0000

    third commit

--- README.md
+++ README.md
@@ -2,7 +2,9 @@ terraform init <br>
 terraform plan <br>
 terraform apply <br>
 
-kubectl get deployment -n sb-graphql-h2-tf <br>
-kubectl get pods -n sb-graphql-h2-tf <br>
+kubectl get deployment -n sb-graphql-h2-ns <br>
+kubectl get pods -n sb-graphql-h2-ns <br>
+
+kubectl get services -n sb-graphql-h2-ns <br>
 
 terraform destroy <br>
--- k8s.tf
+++ k8s.tf
@@ -1,38 +1,41 @@
-resource "kubernetes_namespace" "example" {
+# Namespace
+resource "kubernetes_namespace" "sb-graphql-h2-ns" {
   metadata {
-    name = "sb-graphql-h2-tf"
+        name = "sb-graphql-h2-ns"
   }
 }
 
-resource "kubernetes_deployment" "example" {
+# Deployment
+resource "kubernetes_deployment" "sb-graphql-h2-dp" {
   metadata {
-    name = "sb-graphql-h2"
+    name = "sb-graphql-h2-dp"
+    namespace = kubernetes_namespace.sb-graphql-h2-ns.id
     labels = {
-      test = "sb-graphql-h2"
+      App = "sb-graphql-h2"
     }
-    namespace = "sb-graphql-h2-tf"
   }
 
   spec {
     replicas = 1
-
     selector {
       match_labels = {
-        test = "sb-graphql-h2"
+        App = "sb-graphql-h2"
       }
     }
-
     template {
       metadata {
         labels = {
-          test = "sb-graphql-h2"
+          App = "sb-graphql-h2"
         }
       }
-
       spec {
         container {
           image = "cloudmahesh/sb-graphql-h2:tag1"
           name  = "sb-graphql-h2"
+
+          port {
+            container_port = 8080
+          }
 
           resources {
             limits = {
@@ -47,5 +50,25 @@ resource "kubernetes_deployment" "example" {
         }
       }
     }
+  }
+}
+
+
+#  Service
+resource "kubernetes_service" "sb-graphql-h2-svc" {
+  metadata {
+    name = "sb-graphql-h2-svc"
+    namespace = kubernetes_namespace.sb-graphql-h2-ns.id
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.sb-graphql-h2-dp.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
   }
 }
