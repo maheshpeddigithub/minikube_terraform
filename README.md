5025023b069dd37695cc7b488b908fe8db68204b
Mahesh Peddi <maheshbabu.peddi@googlecloud.corp-partner.google.com>
2024-07-23 00:54:24 +0100

    second commit

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ terraform init <br>
 terraform plan <br>
 terraform apply <br>
 
-kubectl get deployment -n sb-graphql-h2-ns <br>
-kubectl get pods -n sb-graphql-h2-ns <br>
+kubectl get deployment -n sb3-graphql-h2-ns <br>
+kubectl get pods -n sb3-graphql-h2-ns <br>
 
-kubectl get services -n sb-graphql-h2-ns <br>
+kubectl get services -n sb3-graphql-h2-ns <br>
 
 terraform destroy <br>
--- k8s.tf
+++ k8s.tf
@@ -1,17 +1,17 @@
 # Namespace
-resource "kubernetes_namespace" "sb-graphql-h2-ns" {
+resource "kubernetes_namespace" "sb3-graphql-h2-ns" {
   metadata {
-        name = "sb-graphql-h2-ns"
+        name = "sb3-graphql-h2-ns"
   }
 }
 
 # Deployment
-resource "kubernetes_deployment" "sb-graphql-h2-dp" {
+resource "kubernetes_deployment" "sb3-graphql-h2-dp" {
   metadata {
-    name = "sb-graphql-h2-dp"
-    namespace = kubernetes_namespace.sb-graphql-h2-ns.id
+    name = "sb3-graphql-h2-dp"
+    namespace = kubernetes_namespace.sb3-graphql-h2-ns.id
     labels = {
-      App = "sb-graphql-h2"
+      App = "sb3-graphql-h2-h2"
     }
   }
 
@@ -19,19 +19,19 @@ resource "kubernetes_deployment" "sb-graphql-h2-dp" {
     replicas = 1
     selector {
       match_labels = {
-        App = "sb-graphql-h2"
+        App = "sb3-graphql-h2"
       }
     }
     template {
       metadata {
         labels = {
-          App = "sb-graphql-h2"
+          App = "sb3-graphql-h2"
         }
       }
       spec {
         container {
-          image = "cloudmahesh/sb-graphql-h2:tag1"
-          name  = "sb-graphql-h2"
+          image = "cloudmahesh/sb3-graphql-h2:tag1"
+          name  = "sb3-graphql-h2"
 
           port {
             container_port = 8080
@@ -55,14 +55,14 @@ resource "kubernetes_deployment" "sb-graphql-h2-dp" {
 
 
 #  Service
-resource "kubernetes_service" "sb-graphql-h2-svc" {
+resource "kubernetes_service" "sb3-graphql-h2-svc" {
   metadata {
-    name = "sb-graphql-h2-svc"
-    namespace = kubernetes_namespace.sb-graphql-h2-ns.id
+    name = "sb3-graphql-h2-svc"
+    namespace = kubernetes_namespace.sb3-graphql-h2-ns.id
   }
   spec {
     selector = {
-      App = kubernetes_deployment.sb-graphql-h2-dp.spec.0.template.0.metadata[0].labels.App
+      App = kubernetes_deployment.sb3-graphql-h2-dp.spec.0.template.0.metadata[0].labels.App
     }
     port {
       port        = 8080
